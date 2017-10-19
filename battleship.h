d183459091f6a0b22b659bfc0e4bc533fbeb8364
lvndry <monga.4landry@gmail.com>
2017-10-19 10:50:14 +0200

    squelette du projet

---
+++ battleship.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct 
+{
+	int map[10][10]; //la carte du joueur avec ses bateaux : 0:rien;  1:bateau
+	int ennemy_map[10][10]; //la carte qui se remplira au fur et à mesure des actions du joueur
+}Player;
+
+void init(Player*); //LANDRY : permet au joueur de placer ses bateaux
+void attack(Player*); //VINCENT: Demande les coordonées ou tu veux attaquer et dit si tu as touche ou pas un bateau
+void display(Player*); //LEO : Affiche la map du joueur, puis la vision de la map ennemie
+void isAlive(Player*); //CEDRIC : Verifie que le joueur n'a pas perdu
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "battleship.h"
+
+int main(){
+	Player p1, p2;
+	//init(p1);
+	//init(p2);
+}
