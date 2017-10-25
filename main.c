89232c193c9ec5cb14966400ec92f2407a442240
lvndry <monga.4landry@gmail.com>
2017-10-25 15:20:55 +0200

    revision du projet

--- main.c
+++ main.c
@@ -6,20 +6,20 @@ int main(){
 	int pause; //pour les scanf de fin/debut de tour
 
 	printf("PLAYER 1 : press enter");
-	scanf("%d", pause);
+	getchar();
 	init(&p1);
-	printf("This is your map :");
+	printf("This is your map :\n");
 	display(&p1);
 	printf("press enter to end");
-	scanf("%d", pause);
+	getchar();
 
 	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPLAYER 2 : press enter");
-	scanf("%d", pause);
+	getchar();
 	init(&p2);
-	printf("This is your map :");
+	printf("This is your map :\n");
 	display(&p2);
 	printf("press enter to end");
-	scanf("%d", pause);
+	getchar();
 
 	Player* current_player = &p2;
 
@@ -29,7 +29,7 @@ int main(){
 		current_player = ((current_player==&p1)?&p2:&p1);	//le tour revient à l'autre joueur
 		display(&current_player);	//on affiche sa map et ce qu'il sait de la map adverse
 		attack(((current_player==&p1)?&p2:&p1));	//il attaque l'adversaire
-		display(&current_player);	//on réaffiche les map 
+		display(&current_player);	//on réaffiche les map
 
 		printf("press enter to end turn");
 		scanf("%d", pause);
